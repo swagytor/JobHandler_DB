bf80fbe3676fea763a529cad514deb9d3bad6a71
Swagytor <swagytor@yandex.ru>
2023-08-02 04:35:49 +0300

    refactor and added documentation

--- classes/dbmanager.py
+++ classes/dbmanager.py
@@ -2,22 +2,37 @@ import psycopg2
 
 
 class DBManager:
+    """
+    Класс для работы с базами данных,
+    и для получения информации из них
+    """
     conn = None
 
     @classmethod
-    def connect(cls, params):
+    def connect(cls, params) -> None:
+        """
+        Подключается к базе данных
+        :param params: Информация о конфиге базы данных
+        """
         cls.conn = psycopg2.connect(dbname='hh_jobs', **params)
 
     @classmethod
-    def get_companies_and_vacancies_count(cls):
+    def get_companies_and_vacancies_count(cls) -> list[...]:
+        """
+        Получает список всех компаний и количество вакансий у каждой компании
+        """
         with cls.conn.cursor() as cur:
             cur.execute("""SELECT * FROM employers
                         ORDER BY open_vacancies DESC;""")
-            data = [['employer_id', 'employer_name', 'site_url', 'open_vacancies']] + cur.fetchall()
+            data: list[...] = [['employer_id', 'employer_name', 'site_url', 'open_vacancies']] + cur.fetchall()
         return data
 
     @classmethod
-    def get_all_vacancies(cls):
+    def get_all_vacancies(cls) -> list[...]:
+        """
+        Получает список всех вакансий с указанием названия компании,
+        названия вакансии и зарплаты и ссылки на вакансию
+        """
         with cls.conn.cursor() as cur:
             cur.execute(
                 """
@@ -27,13 +42,16 @@ class DBManager:
                 ORDER BY salary DESC, city;
                 """)
 
-            data = [['vacancy_id', 'vacancy_name', 'city', 'salary', 'currency',
-                     'employer_name', 'employment', 'url', 'published_at']] + cur.fetchall()
+            data: list[...] = [['vacancy_id', 'vacancy_name', 'city', 'salary', 'currency',
+                                'employer_name', 'employment', 'url', 'published_at']] + cur.fetchall()
 
         return data
 
     @classmethod
-    def get_avg_salary(cls):
+    def get_avg_salary(cls) -> list[...]:
+        """
+        Получает среднюю зарплату по вакансиям
+        """
         with cls.conn.cursor() as cur:
             cur.execute(
                 """
@@ -41,12 +59,15 @@ class DBManager:
                 FROM vacancies
                 WHERE salary <> 0;
                 """)
-            data = [['average_salary']] + cur.fetchall()
+            data: list[...] = [['average_salary']] + cur.fetchall()
 
         return data
 
     @classmethod
-    def get_vacancies_with_higher_salary(cls):
+    def get_vacancies_with_higher_salary(cls) -> list[...]:
+        """
+        Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям
+        """
         with cls.conn.cursor() as cur:
             cur.execute(
                 """
@@ -57,13 +78,17 @@ class DBManager:
                 ORDER BY salary DESC;
                 """)
 
-            data = [['vacancy_id', 'vacancy_name', 'city', 'salary', 'currency',
-                     'employer_name', 'employment', 'url', 'published_at']] + cur.fetchall()
+            data: list[...] = [['vacancy_id', 'vacancy_name', 'city', 'salary', 'currency',
+                                'employer_name', 'employment', 'url', 'published_at']] + cur.fetchall()
 
         return data
 
     @classmethod
-    def get_vacancies_with_keyword(cls, user_keyword):
+    def get_vacancies_with_keyword(cls, user_keyword: str) -> list[...]:
+        """
+        Получает список всех вакансий, в названии которых содержатся переданные в метод слова
+        :param user_keyword: ключевое слово
+        """
         with cls.conn.cursor() as cur:
             cur.execute(
                 f"""
@@ -74,7 +99,7 @@ class DBManager:
                 ORDER BY salary DESC; 
                 """)
 
-            data = [['vacancy_id', 'vacancy_name', 'city', 'salary', 'currency',
-                     'employer_name', 'employment', 'url', 'published_at']] + cur.fetchall()
+            data: list[...] = [['vacancy_id', 'vacancy_name', 'city', 'salary', 'currency',
+                                'employer_name', 'employment', 'url', 'published_at']] + cur.fetchall()
 
         return data
--- classes/jobhandler.py
+++ classes/jobhandler.py
@@ -1,10 +1,12 @@
 import json
-
 import requests
 
 
 class JobHandler:
-    employers_info = {'employers': [{'employer_id': '1035394',
+    """
+    Класс для работы с API HH.ru для добывания информации о работодателях и вакансиях
+    """
+    employers_info: dict[...] = {'employers': [{'employer_id': '1035394',
                                      'employer_name': 'Красное и Белое'},
                                     {'employer_id': '1740',
                                      'employer_name': 'Яндекс'},
@@ -28,33 +30,43 @@ class JobHandler:
                       }
 
     @classmethod
-    def get_jobs_by_employers_id(cls):
-        params = {'page': 0,
+    def get_jobs_by_employers_id(cls) -> None:
+        """
+        Отправляет запросы и получает информацию о вакансиях выбранных работодателей
+        """
+        params: dict = {'page': 0,
                   'per_page': 100}
-        ids = '&employer_id='.join([employer['employer_id'] for employer in cls.employers_info['employers']])
 
+        # объединяем все id работодателей для совмещённого запроса
+        ids: str = '&employer_id='.join([employer['employer_id'] for employer in cls.employers_info['employers']])
+
+        # получаем информацию о работодателе и количестве активных вакансий
         for employer in cls.employers_info['employers']:
-            employer_response = requests.get(f'https://api.hh.ru/employers/{employer["employer_id"]}').json()
+            employer_response: dict = requests.get(f'https://api.hh.ru/employers/{employer["employer_id"]}').json()
 
-            employer['site_url'] = employer_response.get('site_url')
-            employer['open_vacancies'] = employer_response.get('open_vacancies')
+            employer['site_url']: dict = employer_response.get('site_url')
+            employer['open_vacancies']: int = employer_response.get('open_vacancies')
 
         while True:
-            response = requests.get(f'https://api.hh.ru/vacancies/?employer_id={ids}', params=params).json().get('items')
+            # получаем информацию о вакансиях работодателей
+            response: dict = requests.get(f'https://api.hh.ru/vacancies/?employer_id={ids}',
+                                          params=params).json().get('items')
             if response in (None, []):
                 break
 
             for item in response:
+                # добавляем необходимую информацию о вакансии в словарь
                 cls.employers_info['items'].append(cls.__parse_data(item))
 
             params['page'] += 1
 
-
-
-        return cls.employers_info
-
     @classmethod
-    def __parse_data(cls, item):
+    def __parse_data(cls, item: dict):
+        """
+        Приводит информацию о вакансии в единый вид
+        :param item: информация о вакансии
+        :return: отредактированная информация о вакансии
+        """
         parsed_data = {
             'vacancy_id': item['id'],
             'vacancy_name': item['name'],
@@ -74,7 +86,12 @@ class JobHandler:
         return parsed_data
 
     @staticmethod
-    def __parse_salary(salary_info):
+    def __parse_salary(salary_info: dict) -> int:
+        """
+        Возвращает единый вид информации о зарплате
+        :param salary_info: информация о зарплате
+        :return: отредактированная информация о зарплате
+        """
         if salary_info is None:
             return 0
 
@@ -84,18 +101,27 @@ class JobHandler:
         return salary_info.get('to', 0)
 
     @staticmethod
-    def __parse_currency(currency_info):
+    def __parse_currency(currency_info) -> None | str:
+        """
+        Возвращает единый вид информации о валюте зарплаты
+        :param currency_info: информация о валюте зарплаты
+        :return: отредактированная информация о валюте зарплаты
+        """
         if currency_info is None:
             return None
         return currency_info.get('currency')
 
     @staticmethod
-    def __parse_address(address_info):
+    def __parse_address(address_info) -> None | str:
         if address_info is None:
             return None
         return address_info.get('raw')
 
     @classmethod
-    def log_saving(cls, file_path):
+    def log_saving(cls, file_path: str) -> None:
+        """
+        Сохраняет информацию о вакансиях и работодателе в JSON-файл
+        :param file_path: путь к JSON-файлу
+        """
         with open(file_path, 'w', encoding='utf-8') as json_file:
-            json.dump(cls.employers_info, json_file, indent=2)
+            json.dump(cls.employers_info, json_file, indent=2, ensure_ascii=False)
